877f108b81dc8ec0826cfedf99e4c056fe2debb4
sl154 <102568675+sl154@users.noreply.github.com>
2022-07-29 12:31:44 -0400

    Update cars2017mpg.js

--- cars2017mpg.js
+++ cars2017mpg.js
@@ -113,8 +113,8 @@ function LoadScene() {
     svg.append("text")
         .attr("class", "mainXlabel")
         .attr("text-anchor", "end")
-        .attr("x", parseInt(svg.style("width"))/2)
-        .attr("y", parseInt(svg.style("height")) - 200)
+        .attr("x", parseInt(svg.style("width"))/2 + 50)
+        .attr("y", parseInt(svg.style("height")) - 180)
         .style("font-family", "verdana")
         .style("fill", "black")             
         .text("Average City Mile Per Gallon");    
@@ -122,8 +122,8 @@ function LoadScene() {
     svg.append("text")
         .attr("class", "mainYlabel")
         .attr("text-anchor", "end")
-        .attr("x", 250)
-        .attr("y", 75)
+        .attr("x", 450)
+        .attr("y", -50)
         .attr("dy", ".5em")
         .attr("transform", "rotate(-90)")
         .style("font-family", "verdana")
@@ -133,8 +133,8 @@ function LoadScene() {
     // Annotations
     svg.append("text")
         .attr("class", "annotation")
-        .attr("x", parseInt(svg.style("width")) - 1100)
-        .attr("y", parseInt(svg.style("height")) - 100)
+        .attr("x", parseInt(svg.style("width")) - 1050)
+        .attr("y", parseInt(svg.style("height")) - 120)
         .style("font-family", "verdana")
         .style("fill", "black")             
         .text(function(d) {
@@ -142,8 +142,8 @@ function LoadScene() {
         })
     svg.append("text")
         .attr("class", "annotation")
-        .attr("x", parseInt(svg.style("width")) - 1100)
-        .attr("y", parseInt(svg.style("height")) - 100)
+        .attr("x", parseInt(svg.style("width")) - 1050)
+        .attr("y", parseInt(svg.style("height")) - 120)
         .attr("dy", "1em")
         .style("font-family", "verdana")   
         .style("fill", "blue")     
@@ -152,8 +152,8 @@ function LoadScene() {
         })
     svg.append("text")
     .attr("class", "annotation")
-    .attr("x", parseInt(svg.style("width")) - 1100)
-    .attr("y", parseInt(svg.style("height")) - 100)
+    .attr("x", parseInt(svg.style("width")) - 1050)
+    .attr("y", parseInt(svg.style("height")) - 120)
     .attr("dy", "2em")
     .style("font-family", "verdana")
     .style("fill", "red")             
@@ -162,8 +162,8 @@ function LoadScene() {
     })
     svg.append("text")
     .attr("class", "annotation")
-    .attr("x", parseInt(svg.style("width")) - 1100)
-    .attr("y", parseInt(svg.style("height")) - 100)
+    .attr("x", parseInt(svg.style("width")) - 1050)
+    .attr("y", parseInt(svg.style("height")) - 120)
     .attr("dy", "3em")
     .style("font-family", "verdana")   
     .style("fill", "green")          
@@ -172,8 +172,8 @@ function LoadScene() {
     })
     svg.append("text")
         .attr("class", "annotation")
-        .attr("x", parseInt(svg.style("width")) - 1100)
-        .attr("y", parseInt(svg.style("height")) - 100)
+        .attr("x", parseInt(svg.style("width")) - 1050)
+        .attr("y", parseInt(svg.style("height")) - 80)
         .style("font-family", "verdana")
         .style("fill", "green")             
         .text(function(d) {
@@ -183,12 +183,12 @@ function LoadScene() {
     svg.append("text")
         .attr("class", "annotation")
         .attr("x", 700)
-        .attr("y", parseInt(svg.style("height")) - 300)
+        .attr("y", parseInt(svg.style("height")) - 400)
         .style("font-family", "verdana")
         .style("fill", "red")             
         .text(function(d) {
             if(null == SelectedMake)
-                return "* Hover and click on the circle to see details";
+                return "* Hover over the circle to see details. Click to see select brand.";
     });
 }
 
